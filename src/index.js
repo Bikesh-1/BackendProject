10f63762a254fd8dd6c0f18917cf990e113ce4ea
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-05-28 23:43:13 +0530

    feat: add cookie-parser and cors middleware, implement error handling classes

--- src/index.js
+++ src/index.js
@@ -7,4 +7,14 @@ dotenv.config({
     path:'./env'
 });
 
-connectDB();
+connectDB()
+
+.then(() => {
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("Error connecting to the database:", err);
+    process.exit(1);
+})
