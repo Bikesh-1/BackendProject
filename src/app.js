02a19519931e82d008ed90f902a5fb1735d283cf
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-05-30 11:51:39 +0530

    feat: implement user registration route and controller; add async error handling

--- src/app.js
+++ src/app.js
@@ -18,6 +18,14 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static('public'));
 app.use(cookieParser());
 
+//routes
+import userRouter from './routes/user.routes.js';
 
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+// http://localhost:3000/api/v1/users/register
+// http://localhost:3000/api/v1/users/login
 
 export {app}
